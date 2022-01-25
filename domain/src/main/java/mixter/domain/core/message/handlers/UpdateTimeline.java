a69cc1d356449beab55a8d9a7ee6186f46d8ef8c
mbimbij <joseph.mbimbi@gmail.com>
2022-01-25 20:32:17 +0100

    Resolve test

--- domain/src/main/java/mixter/domain/core/message/handlers/UpdateTimeline.java
+++ domain/src/main/java/mixter/domain/core/message/handlers/UpdateTimeline.java
@@ -1,6 +1,20 @@
 package mixter.domain.core.message.handlers;
 
+import mixter.domain.core.message.TimelineMessageProjection;
+import mixter.domain.core.message.TimelineMessageRepository;
+import mixter.domain.core.message.events.MessageQuacked;
+
 public class UpdateTimeline {
-    public UpdateTimeline() {
+    private final TimelineMessageRepository timelineRepository;
+
+    public UpdateTimeline(TimelineMessageRepository timelineRepository) {
+        this.timelineRepository = timelineRepository;
+    }
+
+    public void apply(MessageQuacked messageQuacked) {
+        timelineRepository.save(new TimelineMessageProjection(messageQuacked.getAuthorId(),
+                messageQuacked.getAuthorId(),
+                messageQuacked.getMessage(),
+                messageQuacked.getMessageId()));
     }
 }
