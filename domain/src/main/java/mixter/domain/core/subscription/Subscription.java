35d0b99dcbc7ad1cd3affa505cbb96ccdca09522
mbimbij <joseph.mbimbi@gmail.com>
2022-01-25 21:03:20 +0100

    Resolve test

--- domain/src/main/java/mixter/domain/core/subscription/Subscription.java
+++ domain/src/main/java/mixter/domain/core/subscription/Subscription.java
@@ -1,13 +1,48 @@
 package mixter.domain.core.subscription;
 
 import mixter.doc.Aggregate;
+import mixter.doc.Projection;
+import mixter.domain.DecisionProjectionBase;
+import mixter.domain.Event;
 import mixter.domain.EventPublisher;
 import mixter.domain.core.subscription.events.UserFollowed;
+import mixter.domain.core.subscription.events.UserUnfollowed;
 import mixter.domain.identity.UserId;
+
+import java.util.List;
 
 @Aggregate
 public class Subscription {
+
+    private final DecisionProjection projection;
+
+    public Subscription(List<Event> history) {
+        projection = new DecisionProjection(history);
+    }
+
     public static void follow(UserId follower, UserId followee, EventPublisher eventPublisher) {
         eventPublisher.publish(new UserFollowed(new SubscriptionId(follower, followee)));
+    }
+
+    public void unfollow(EventPublisher eventPublisher) {
+        eventPublisher.publish(new UserUnfollowed(projection.getSubscriptionId()));
+    }
+
+    @Projection
+    private class DecisionProjection extends DecisionProjectionBase {
+        private SubscriptionId subscriptionId;
+
+        public DecisionProjection(List<Event> history) {
+            super.register(UserFollowed.class, this::apply);
+            history.forEach(this::apply);
+        }
+
+        public void apply(UserFollowed event) {
+            subscriptionId = event.getSubscriptionId();
+        }
+
+        public SubscriptionId getSubscriptionId() {
+            return subscriptionId;
+        }
     }
 }
