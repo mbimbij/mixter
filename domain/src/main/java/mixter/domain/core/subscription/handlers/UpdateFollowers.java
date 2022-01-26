00fb63ce76ebfe0f72845bac9bd47ed77c0ebbef
mbimbij <joseph.mbimbi@gmail.com>
2022-01-26 05:52:02 +0100

    Resolve test

--- domain/src/main/java/mixter/domain/core/subscription/handlers/UpdateFollowers.java
+++ domain/src/main/java/mixter/domain/core/subscription/handlers/UpdateFollowers.java
@@ -3,6 +3,7 @@ package mixter.domain.core.subscription.handlers;
 import mixter.doc.Handler;
 import mixter.domain.core.subscription.FollowerRepository;
 import mixter.domain.core.subscription.events.UserFollowed;
+import mixter.domain.core.subscription.events.UserUnfollowed;
 import mixter.domain.identity.UserId;
 
 @Handler
@@ -18,5 +19,11 @@ public class UpdateFollowers {
         UserId followee = userFollowed.getSubscriptionId().getFollowee();
         UserId follower = userFollowed.getSubscriptionId().getFollower();
         repository.saveFollower(followee, follower);
+    }
+
+    public void apply(UserUnfollowed userUnfollowed) {
+        UserId followee = userUnfollowed.getSubscriptionId().getFollowee();
+        UserId follower = userUnfollowed.getSubscriptionId().getFollower();
+        repository.removeFollower(followee, follower);
     }
 }
